dfc01070a310be4e37add529e8c484a0990940a4
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-27 17:55:20 +0500

    created middleware

---
+++ middleware/database.ts
@@ -0,0 +1,27 @@
+import {MongoClient} from 'mongodb';
+import nextConnect from 'next-connect';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client=new MongoClient(`{${process.env.MONGODB_CONNECTION}}`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+
+const database=async(req,res,next)=>{ 
+    if(!client.isConnected()) 
+        await client.connect();
+
+    req.dbClient=client;
+    req.db=client.db("MCT");
+
+    return next();
+}
+
+const middleware=nextConnect();
+
+middleware.use(database);
+
+export default middleware;
--- pages/index.tsx
+++ pages/index.tsx
@@ -38,14 +38,14 @@ const Home: React.SFC<HomeProps> = () => {
 
   const [results, setResults] = React.useState(data);
 
-  const onChange=(e)=>{
-    const data={...results};
+  const onChange = (e) => {
+    const data = { ...results };
 
-    let name=e.target.name;
-    let resultType=name.split(" ")[0].toLowerCase();
-    let resultMacro=name.split(" ")[1].toLowerCase();
+    let name = e.target.name;
+    let resultType = name.split(" ")[0].toLowerCase();
+    let resultMacro = name.split(" ")[1].toLowerCase();
 
-    data[resultMacro][resultType]=e.target.value;
+    data[resultMacro][resultType] = e.target.value;
 
     setResults(data);
   }
@@ -66,7 +66,7 @@ const Home: React.SFC<HomeProps> = () => {
               <h1 className="text-4xl">Macro Compliacne Tracker</h1>
             </div>
           </div>
-          
+
           <div className="flex text-center">
             <div className="w-1/3 bg-gray-200 p-4">Previous Day</div>
             <div className="w-1/3 p-4">Today</div>
@@ -79,15 +79,21 @@ const Home: React.SFC<HomeProps> = () => {
             <Result results={results.carbs} />
             <Result results={results.fat} />
             <Result results={results.protein} />
-          
+
           </div>
 
           <div className="flex">
             <MCT data={results} item="Total" onChange={onChange} />
             <MCT data={results} item="Target" onChange={onChange} />
             <MCT data={results} item="Variant" onChange={onChange} />
+          </div>
 
-
+          <div className="flex text-center">
+            <div className="w-full m-4">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Save
+              </button>
+            </div>
           </div>
 
         </div>
