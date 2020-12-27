d6004c1cfdb8a6f01d2f7d3094320bb54166d886
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-27 17:04:14 +0500

    created MCTForm component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Nav from '../components/nav';
-import Result from '../components/result';
+import Result from '../components/result'
 
 let data = {
   calories: {
